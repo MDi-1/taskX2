345784a516bfaab94163e42dee50e74ea5b171d9
MDi <calendarzyk@gmail.com>
2022-04-01 10:46:35 +0200

    before first run 2

--- src/main/java/com/crud/taskX2/controller/TaskController.java
+++ src/main/java/com/crud/taskX2/controller/TaskController.java
@@ -1,8 +1,8 @@
 package com.crud.taskX2.controller;
 
 import com.crud.taskX2.domain.TaskDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,18 +14,22 @@ public class TaskController {
         return new ArrayList<>();
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public TaskDto getTask(Long taskId) {
         return new TaskDto(1L, "test title", "test_content");
     }
 
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
     public void deleteTask(Long taskId) {
 
     }
 
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
     public TaskDto updateTask(TaskDto taskDto) {
         return new TaskDto(1L, "Edited test title", "Test content");
     }
 
+    @RequestMapping(method = RequestMethod.POST, value = "createTask", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void createTask(TaskDto taskDto) {
 
     }
