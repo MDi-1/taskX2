12506dcef9d67b0aea89ac63a34bb963870a1ec6
MDi <calendarzyk@gmail.com>
2022-06-07 12:51:40 +0200

    class refactored

--- src/main/java/com/crud/taskX2/client/emptyClass.java
+++
@@ -1,7 +0,0 @@
-package com.crud.taskX2.client;
-
-public class emptyClass {
-    void function() {
-        System.out.println("class expanded to empty function with 'sout'.");
-    }
-}
---
+++ src/main/java/com/crud/taskX2/client/templateClass.java
@@ -0,0 +1,4 @@
+package com.crud.taskX2.client;
+
+public class templateClass {
+}
